e2cd6d6fa2722f744bc99f5f367bc83f3f18054f
LASHMEE <cbmlashmee2006@gmail.com>
2026-08-04 22:16:26 +0530

    Add Easy Java Solution from LeetCode

---
+++ LeetCode/Java/Easy/4107. Find Missing Elements/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        int max=Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int x :nums){
+            set.add(x);
+            min=Math.min(min,x);
+            max=Math.max(max,x);
+        }
+        List<Integer> ans = new ArrayList<>();
+        for(int i=min;i<=max;i++){
+            if(!set.contains(i)) ans.add(i);
+        }
+        return ans;
+    }
+}
