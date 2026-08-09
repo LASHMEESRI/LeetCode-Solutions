627125e1dda848aa16f6636428d503c5c436a941
LASHMEE <cbmlashmee2006@gmail.com>
2026-08-09 20:15:42 +0530

    Add Java Solution to Patterns/Math

---
+++ LeetCode/Patterns/Math/7. Reverse Integer/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverse(int x) {
+        
+        long rem=0;
+        while(x!=0){
+            int dig=x%10;
+            rem = rem*10+dig;
+            x/=10;
+        }
+        if (rem>Integer.MAX_VALUE||rem<Integer.MIN_VALUE) return 0;
+        return (int)rem;
+    }
+}
