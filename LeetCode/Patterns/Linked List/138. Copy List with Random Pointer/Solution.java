846811bf831b9e170525804c2b3064e20555ea96
LASHMEE <cbmlashmee2006@gmail.com>
2026-08-13 23:37:43 +0530

    Add Java Solution to Patterns/Linked List

--- LeetCode/Patterns/Linked List/138. Copy List with Random Pointer/Solution.java
+++ LeetCode/Patterns/Linked List/138. Copy List with Random Pointer/Solution.java
@@ -1,61 +1,22 @@
-/*
-// Definition for a Node.
-class Node {
-    int val;
-    Node next;
-    Node random;
+class Solution {
+    public Node copyRandomList(Node head) {
+        Map<Node, Node> hashMap = new HashMap<>();
+        Node cur = head;
 
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
+        while (cur != null) {
+            hashMap.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+
+        cur = head;
+
+        while (cur != null) {
+            Node copy = hashMap.get(cur);
+            copy.next = hashMap.get(cur.next);
+            copy.random = hashMap.get(cur.random);
+            cur = cur.next;
+        }
+
+        return hashMap.get(head);        
     }
-}
-*/
-class Solution{
-public RandomListNode copyRandomList(RandomListNode head) {
-  RandomListNode iter = head, next;
-
-  // First round: make copy of each node,
-  // and link them together side-by-side in a single list.
-  while (iter != null) {
-    next = iter.next;
-
-    RandomListNode copy = new RandomListNode(iter.label);
-    iter.next = copy;
-    copy.next = next;
-
-    iter = next;
-  }
-
-  // Second round: assign random pointers for the copy nodes.
-  iter = head;
-  while (iter != null) {
-    if (iter.random != null) {
-      iter.next.random = iter.random.next;
-    }
-    iter = iter.next.next;
-  }
-
-  // Third round: restore the original list, and extract the copy list.
-  iter = head;
-  RandomListNode pseudoHead = new RandomListNode(0);
-  RandomListNode copy, copyIter = pseudoHead;
-
-  while (iter != null) {
-    next = iter.next.next;
-
-    // extract the copy
-    copy = iter.next;
-    copyIter.next = copy;
-    copyIter = copy;
-
-    // restore the original list
-    iter.next = next;
-
-    iter = next;
-  }
-
-  return pseudoHead.next;
-}
 }
