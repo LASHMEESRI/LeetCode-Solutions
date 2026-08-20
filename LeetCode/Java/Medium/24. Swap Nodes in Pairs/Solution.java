b3595d8f4eaf6cf29206552e1b8264f949303815
LASHMEE <cbmlashmee2006@gmail.com>
2026-08-20 21:24:50 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/24. Swap Nodes in Pairs/Solution.java
+++ LeetCode/Java/Medium/24. Swap Nodes in Pairs/Solution.java
@@ -10,21 +10,17 @@
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        ListNode dummy = new ListNode(0, head);
-        ListNode prev = dummy, cur = head;
-
-        while (cur != null && cur.next != null) {
-            ListNode npn = cur.next.next;
-            ListNode second = cur.next;
-
-            second.next = cur;
-            cur.next = npn;
-            prev.next = second;
-
-            prev = cur;
-            cur = npn;
+        ListNode dummy = new ListNode(0);
+        dummy.next=head;
+        ListNode point =dummy;
+        while(point.next!=null&&point.next.next!=null){
+            ListNode swap1=point.next;
+            ListNode swap2=point.next.next;
+            swap1.next=swap2.next;
+            swap2.next=swap1;
+            point.next=swap2;
+            point=swap1;
         }
-
-        return dummy.next; 
+        return dummy.next;
     }
 }
